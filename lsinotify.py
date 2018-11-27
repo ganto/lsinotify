0a0b2514fae3a4a32545e20ef941fc7aa14829e8
Reto Gantenbein <reto.gantenbein@linuxmonk.ch>
2018-11-28 00:39:04 +0100

    Fix uid filter, use debugfs for ext, get_path() via nsenter

--- lsinotify.py
+++ lsinotify.py
@@ -18,14 +18,12 @@ class MountManager():
 
     def __init__(self, pid):
         fs_mounts = subprocess.check_output(
-            ["nsenter", "--target", str(pid), "--mount", "--uts", "--ipc", "--net", "--pid", "--", "mount"])
+            ["nsenter", "--target", str(pid), "--mount", "--pid", "--",
+             "mount"]
+        )
         for mount in fs_mounts.split('\n')[:-1]:
-            #            if not mount:
-            #                continue
             try:
-                self.mounts.append(Mount(mountpoint=mount.split()[2],
-                                         fstype=mount.split()[4],
-                                         pid=pid))
+                self.mounts.append(Mount(mountpoint=mount.split()[2], pid=pid))
             except subprocess.CalledProcessError:
                 # this will happen when the mount point cannot be read, so far
                 # this only happend for a gvfs mount, therefore skip for now
@@ -33,33 +31,90 @@ class MountManager():
 
     def get_mount_by_device_number(self, device_number):
         for mount in self.mounts:
-            #            print("get_mount_by_device_number(): mount.device_number = %x/%d <-> device_number = %x/%d" % (mount.device_number, mount.device_number, device_number, device_number))
             if mount.device_number == device_number:
                 return mount
         return None
 
 
 class Mount():
-    mountpoint = None
-    fstype = None
+    """
+    File system mount
+    """
+    device = None
     device_number = None
+    fstype = None
+    mountpoint = None
+    pid = None
 
-    def __init__(self, mountpoint, fstype, pid):
+    def __init__(self, mountpoint, pid):
         self.mountpoint = mountpoint
-        self.fstype = fstype
-        self.device_number = int(subprocess.check_output(["nsenter", "--target", str(
-            pid), "--mount", "--uts", "--ipc", "--net", "--pid", "--", "stat", "-c %d", "%s" % mountpoint]))
-#        print("%s" % self)
+        self.fstype = subprocess.check_output(
+            ["nsenter", "--target", str(pid), "--mount", "--pid", "--",
+             "findmnt", "--noheadings", "--output", "FSTYPE", "%s" %
+             self.mountpoint]
+        ).strip()
+        self.device = subprocess.check_output(
+            ["nsenter", "--target", str(pid), "--mount", "--pid", "--",
+             "findmnt", "--noheadings", "--output", "SOURCE", "%s" %
+             self.mountpoint]
+        ).strip()
+        self.device_number = int(subprocess.check_output(
+            ["nsenter", "--target", str(pid), "--mount", "--pid", "--",
+             "stat", "-c %d", "%s" % mountpoint])
+        )
+        # store pid-reference for mount
+        # TODO: replace with proper namespace handling
+        self.pid = pid
 
-    def get_device_number(self):
-        return self.device_number
-
-    def get_mountpoint(self):
-        return self.mountpoint
+        debug("%s" % self)
 
     def __str__(self):
         return "Mount({'mountpoint': '%s', 'fstype': '%s', 'device_number': %x})" % \
             (self.mountpoint, self.fstype, self.device_number)
+
+    def get_device(self):
+        return self.device
+
+    def get_device_number(self):
+        return self.device_number
+
+    def get_path(self, inode):
+        # debugfs is much faster than find, but only available for ext2/3/4
+        if self.fstype in ['ext2', 'ext3', 'ext4']:
+            cmd = ["nsenter", "--target", str(self.pid), "--mount", "--pid",
+                   "--", "debugfs", self.get_device(), "-R", "ncheck %s" %
+                   inode]
+            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            # output will contain something like (in one line):
+            #
+            #   debugfs 1.44.2 (14-May-2018)\n
+            #   Inode\tPathname\n
+            #   15\t/lost+found\n
+            #
+            path_tuple = output.split('\n')[2].split('\t')
+            if len(path_tuple) == 2:
+                # the path is relative to the mountpoint and starts with '/'
+                rel_path = path_tuple[1].lstrip('/')
+                path = os.path.join(self.mountpoint, rel_path)
+            else:
+                # if the inode is the mountpoint itself, the Pathname is empty
+                path = self.mountpoint
+        else:
+            cmd = ["nsenter", "--target", str(self.pid), "--mount", "--pid",
+                   "--", "find", "%s" % self.mountpoint, "-xdev", "-inum",
+                   "%d" % inode, "-print"]
+            path_list = subprocess.check_output(cmd).split('\n')[:-1]
+            # if the inode is the mountpoint itself, it can happen, that
+            # multiple paths are returned(?!)
+            path = path_list[0]
+
+        debug("Mount.get_path() : cmd = '%s'" % " ".join(cmd))
+        debug("Mount.get_path() : path = %s" % path)
+
+        return path
+
+    def get_mountpoint(self):
+        return self.mountpoint
 
 
 class FsNotifyHandle():
@@ -93,23 +148,20 @@ class FsNotifyHandle():
             elif item.startswith('f_handle'):
                 self.fhandle = int(item.split(':')[1], 16)  # hex
 
+        debug("FsNotifyHandle(%s)" % self)
+
     def __str__(self):
-        return "inotify wd:%x ino:%x sdev:%x mask:%x ignored_mask:%x fhandle-bytes:%x fhandle-type:%x f_handle:%x" % \
-            (self.watch_descriptor, self.inode, self.source_device, self.mask,
-             self.ignored_mask, self.fhandle_bytes, self.fhandle_type, self.fhandle)
+        return "inotify wd:%x ino:%x sdev:%x " % \
+            (self.watch_descriptor, self.inode, self.source_device) + \
+            "mask:%x ignored_mask:%x fhandle-bytes:%x fhandle-type:%x " % \
+            (self.mask, self.ignored_mask, self.fhandle_bytes,
+             self.fhandle_type) + "f_handle:%x" % self.fhandle
 
     def get_inode(self):
         return self.inode
 
     def get_path(self, mount):
-        #        print("DEBUG : FsNotifyHandle.get_path(mount='%s'): mountpoint = %s") % \
-        #              (mount, mount.mountpoint)
-        cmd = ["find", "%s" % mount.mountpoint, "-xdev",
-               "-inum", "%d" % self.inode, "-print"]
-#        print("DEBUG : FsNotifyHandle: cmd = '%s'" % " ".join(cmd))
-        path_list = subprocess.check_output(cmd).split('\n')[:-1]
-#        print("DEBUG : FsNotifyHandle: path_list = %s" % path_list)
-        return path_list[0]
+        return mount.get_path(self.inode)
 
     def get_source_device(self):
         return self.source_device
@@ -119,24 +171,29 @@ class FsNotifyHandle():
 
 
 class Process():
+    """
+    Represents an Linux process with the given PID (process id).
+    """
 
-    def __init__(self, pid):
+    def __init__(self, pid=os.getpid()):
         self.pid = int(pid)
+
         # this will throw exceptions if process has terminated in the meantime
-        with open(os.path.join('/proc', str(self.pid), 'cmdline'), 'r') as cmd_fd:
-            self.cmdline = cmd_fd.readline().strip('\n').replace('\0', ' ')
-        with open(os.path.join('/proc', str(self.pid), 'uid_map'), 'r') as uid_fd:
-            uid_map = uid_fd.readlines()
-#        print("DEBUG : pid = %d, uid_map = %s" % (pid, uid_map))
-        if len(uid_map) > 0:
-            self.real_uid = int(uid_map[0].strip('\n').split()[0])
-        else:
-            with open(os.path.join('/proc', str(self.pid), 'loginuid'), 'r') as loginuid_fd:
-                self.real_uid = int(loginuid_fd.readlines()[0].strip('\n'))
+        with open(os.path.join('/proc',
+                               str(self.pid),
+                               'cmdline'), 'r') as cmd_fd:
+            # cmdline arguments are \0 seperated
+            self.cmdline = cmd_fd.readline().replace('\0', ' ').strip()
+
+        self.uid = int(subprocess.check_output(
+            ["ps", "--no-headings", "-o", "uid", "-p", "%d" % self.pid]
+        ).strip())
+
+        debug("%s" % self)
 
     def __str__(self):
-        return "Process({'cmdline': '%s', 'pid': %d, 'real_uid': %d})" % \
-            (self.cmdline, self.pid, self.real_uid)
+        return "Process({'cmdline': '%s', 'pid': %d, 'uid': %d})" % \
+            (self.cmdline, self.pid, self.uid)
 
     def get_cmdline(self):
         return self.cmdline
@@ -144,9 +201,10 @@ class Process():
     def get_fsnotify_watches(self):
         watches = list()
         fdinfo = os.path.join('/proc', str(self.pid), 'fdinfo')
+
         for fd in os.listdir(fdinfo):
-            with open(os.path.join(fdinfo, str(fd)), 'r') as fdfd:
-                content = fdfd.readlines()
+            with open(os.path.join(fdinfo, str(fd)), 'r') as fdinfo_fd:
+                content = fdinfo_fd.readlines()
             for line in content:
                 if "inotify" in line:
                     info = {'fd': fd}
@@ -159,8 +217,8 @@ class Process():
     def get_pid(self):
         return self.pid
 
-    def get_real_uid(self):
-        return self.real_uid
+    def get_uid(self):
+        return self.uid
 
 
 def debug(msg):
@@ -181,13 +239,13 @@ def main(argv=None):
 #                      help="format output (default: 'column')")
 #    parser.add_option('-l', '--limit', action='store', type='str', metavar='OPT=VAL',
 #                      help='limit output to match given option/value')
-#    parser.add_option('-n', '--no-headers', action='store_true', default=False,
-#                      help='do not print column headers')
+    parser.add_option('-n', '--no-headers', action='store_true', default=False,
+                      help='do not print column headers')
 #    parser.add_option('-o', '--options', action='store', type='str', metavar='STRING',
 #                      help='only show given option columns')
     parser.add_option('-p', '--pids', action='store', type='str', metavar='NUM[,NUM]',
                       help='restrict to list of process IDs')
-#    parser.add_options('0s', '--sort-by', action='store', type='str', metavar='OPTION',
+#    parser.add_options('-s', '--sort-by', action='store', type='str', metavar='OPTION',
 #                       help='sort output by given column')
     parser.add_option('-u', '--uid', action='store', type='int', metavar='NUM',
                       help='examine processes of given user ID (root only!)')
@@ -200,6 +258,11 @@ def main(argv=None):
         global DEBUG
         DEBUG = True
 
+    if options.no_headers:
+        headers = False
+    else:
+        headers = True
+
     if options.pids:
         pids = [int(pid) for pid in options.pids.split(',')]
     else:
@@ -207,73 +270,23 @@ def main(argv=None):
 
     debug("checking PIDs: %s" % ", ".join(str(pid) for pid in pids))
 
-    proc_self = Process(os.getpid())
-    uid = proc_self.get_real_uid()
+    # by default use UID of executing user
+    uid = int(Process().get_uid())
     if options.all:
         uid = None
     if options.uid:
         uid = options.uid
 
-    print_inotify_watches(get_inotify_watches(pids, uid))
+    print_inotify_watches(get_inotify_watches(pids=pids,
+                                              restrict_uid=uid),
+                          show_headers=headers)
 
 
-def get_inotify_watches(pids, uid=None):
+def get_inotify_watches(pids, restrict_uid=None):
     inotify_watches = {}
     for pid in pids:
         try:
             proc = Process(pid)
-
-            # restrict to given uid
-            if uid and (proc.get_real_uid() != int(uid)):
-                debug("skipping process (PID %d) due to UID restriction" % pid)
-                continue
-
-            watches = proc.get_fsnotify_watches()
-            if len(watches) == 0:
-                debug("no fsnotify watches found for process (PID %d)" % pid)
-                continue
-
-#            print("\nINFO : Cmdline(Pid %d): %s" % (proc.pid, proc.cmdline()))
-
-            # get mount manager per namespace
-            mm = MountManager(proc.pid)
-            for watch in watches:
-                fsnotify = watch['fsnotify']
-                sdev = fsnotify.get_source_device()
-                mount = mm.get_mount_by_device_number(sdev)
-                if not mount:
-                    # no idea what's going on here, can't find the corresponding device
-                    print("WARN : Cannot find sdev = %x" % sdev)
-                    continue
-                wd = fsnotify.get_watch_descriptor()
-                path = fsnotify.get_path(mount)
-                uid = proc.get_real_uid()
-                key = (wd, sdev, path, uid)
-
-                # create a unique hash to de-duplicate fsnotify entries
-                inotify_hash = hash(key)
-
-                if inotify_hash in inotify_watches:
-                    # append process if entry already exists
-                    inotify_watches[inotify_hash]['procs'].append({'pid': proc.get_pid(),
-                                                                   'cmdline': proc.get_cmdline(),
-                                                                   'fd': watch['fd'],
-                                                                   'uid': uid})
-                else:
-                    # create new entry
-                    watch = {
-                        'watch_descriptor': wd,
-                        'source_device': sdev,
-                        'path': path,
-                        'inode': fsnotify.get_inode(),
-                        'procs': [{
-                            'pid': proc.get_pid(),
-                            'cmdline': proc.get_cmdline(),
-                            'fd': watch['fd'],
-                            'uid': uid,
-                        }]
-                    }
-                    inotify_watches[inotify_hash] = watch
 
         # ignore terminated processes
         except IOError:
@@ -285,25 +298,105 @@ def get_inotify_watches(pids, uid=None):
                       pid, file=sys.stderr)
                 sys.exit(1)
 
+        debug("get_inotify_watches(): pid = %d, restrict_uid = %s, uid = %d" %
+              (pid, restrict_uid, proc.get_uid()))
+
+        # restrict to given uid
+        if (restrict_uid is not None) and (restrict_uid != proc.get_uid()):
+            debug("get_inotify_watches(): skipping process (PID %d)" % pid +
+                  "due to UID restriction")
+            continue
+
+        try:
+            watches = proc.get_fsnotify_watches()
+        # ignore terminated processes
+        except IOError:
+            continue
+
+        if len(watches) == 0:
+            debug("get_inotify_watches(): no fsnotify watches found for " +
+                  "process (PID %d)" % pid)
+            continue
+
+        # get mount manager per namespace
+        mm = MountManager(proc.pid)
+        for watch in watches:
+            fsnotify = watch['fsnotify']
+            sdev = fsnotify.get_source_device()
+            mount = mm.get_mount_by_device_number(sdev)
+            if not mount:
+                # no idea what's going on here, can't find the corresponding
+                # device
+                print("WARN : Cannot find sdev = %x" % sdev)
+                continue
+
+            wd = fsnotify.get_watch_descriptor()
+            path = fsnotify.get_path(mount)
+            uid = proc.get_uid()
+            key = (wd, sdev, path, uid)
+
+            # create a unique hash to de-duplicate fsnotify entries
+            inotify_hash = hash(key)
+
+            if inotify_hash in inotify_watches:
+                # append fd/process details if entry already exists
+                fd_info = {'pid': proc.get_pid(),
+                           'cmdline': proc.get_cmdline(),
+                           'fd': watch['fd'],
+                           'uid': uid}
+
+                debug("get_inotify_watches(): update inotify watch entry " +
+                      "'%s': %s" % (inotify_hash, fd_info))
+
+                inotify_watches[inotify_hash]['procs'].append(fd_info)
+
+            else:
+                # create new entry
+                watch = {
+                    'watch_descriptor': wd,
+                    'source_device': sdev,
+                    'path': path,
+                    'inode': fsnotify.get_inode(),
+                    'procs': [{
+                        'pid': proc.get_pid(),
+                        'cmdline': proc.get_cmdline(),
+                        'fd': watch['fd'],
+                        'uid': uid,
+                    }]
+                }
+
+                debug("get_inotify_watches(): create inotify watch entry " +
+                      "'%s': %s" % (inotify_hash, watch))
+
+                inotify_watches[inotify_hash] = watch
+
     return inotify_watches
 
 
 def get_max_user_watches():
-    return int(subprocess.check_output(["cat", "/proc/sys/fs/inotify/max_user_watches"]).strip())
+    return int(subprocess.check_output(
+        ["cat", "/proc/sys/fs/inotify/max_user_watches"]
+    ).strip())
 
 
-def print_inotify_watches(watches, sort_by='watch_descriptor', summary=True):
+def print_inotify_watches(watches,
+                          show_headers=True,
+                          sort_by='watch_descriptor',
+                          summary=True):
+
+    # sort collected watch entries
     output_list = sorted(watches.values(), key=itemgetter(sort_by))
 
     if len(output_list) > 0:
-        print("{:<6} {:<7} {:<10} {:<90} {:<4} {:<6} {:<6} {:<50}".format(
-            'WD', 'SDEV', 'INODE', 'PATH', 'FD', 'PID', 'UID', 'CMD'
-        ))
+        if show_headers:
+            print("{:<6} {:<7} {:<10} {:<90} {:<4} {:<6} {:<8} {:<50}".format(
+                'WD', 'SDEV', 'INODE', 'PATH', 'FD', 'PID', 'UID', 'CMD'
+            ))
 
         for item in output_list:
             for proc_idx in range(0, len(item['procs'])):
-                # print(item)
-                print("{:<6} {:<7} {:<10} {:<90} {:<4} {:<6} {:<6} {:<50}".format(
+                # TODO: make width depending on item length
+                print("{:<6} {:<7} {:<10} {:<90} {:<4} {:<6} {:<8} {:<50}".format(
                     hex(item['watch_descriptor']),
                     hex(item['source_device']),
                     hex(item['inode']),
